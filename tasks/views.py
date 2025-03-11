f8c57f18935b5f61499d50b33f071382cb46e306
Abir Hasan Piash <abirhasanpiash@gmail.com>
2025-03-11 12:35:24 +0600

    t
    exit
    exit()
    t
    
    exit
    exit()

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm, TaskModelForm
+from tasks.models import Employee, Task, TaskDetail, Project
+from datetime import date
+from django.db.models import Q, Count, Max, Min, Avg
+
+
+# Create your views here.
+
 
 def manager_dashboard(request):
     return render(request, "dashboard/manager-dashboard.html")
 
+
 def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
 
+
 def test(request):
+    names = ["Mahmud", "Ahamed", "John", "Mr. X"]
+    count = 0
+    for name in names:
+        count += 1
     context = {
-        "dream": "I am comming to conqure"
+        "names": names,
+        "age": 23,
+        "count": count
     }
-    return render(request, "test.html", context)
+    return render(request, 'test.html', context)
+
+
+def create_task(request):
+    # employees = Employee.objects.all()
+    form = TaskModelForm()  # For GET
+
+    if request.method == "POST":
+        form = TaskModelForm(request.POST)
+        if form.is_valid():
+
+            """ For Model Form Data """
+            form.save()
+
+            return render(request, 'task_form.html', {"form": form, "message": "task added successfully"})
+
+    context = {"form": form}
+    return render(request, "task_form.html", context)
+
+
+def view_task(request):
+    projects = Project.objects.annotate(
+        num_task=Count('task')).order_by('num_task')
+    return render(request, "show_task.html", {"projects": projects})
